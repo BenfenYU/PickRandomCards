56b44a470648a7e668460ceb2d7fe04532ed6751
Jingbo Yu <JYu20@slb.com>
2023-04-20 15:27:47 +0800

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PickRandomCards;
+
+Console.WriteLine("Hello, World!");
+
+while (true)
+{
+    Console.Write("Enter the number of cards to pick: ");
+    string line = Console.ReadLine();
+
+    if (int.TryParse(line, out int numberOfCards))
+    {
+        foreach (string s in CardPicker.PickSomeCards(numberOfCards))
+        {
+            Console.WriteLine(s);
+        }
+    }
+    else
+    {
+        Console.WriteLine("invalid input!");
+    }
+}
